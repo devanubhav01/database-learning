d848726fea72ebf0a185605e33e9672c89a665b2
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-09-02 15:49:50 +0000

    Sep 2, 2026, 9:19 PM

--- MySQL/F1.sql
+++ MySQL/F1.sql
@@ -8,6 +8,7 @@ id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(100) UNIQUE NOT NULL,
     gender ENUM('Male', 'Female', 'Other'),
     date_of_birth DATE,
-);
+
 
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
