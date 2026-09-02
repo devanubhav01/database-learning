d457280358f471d136a2ea59b6f02706a9187f91
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-09-02 15:49:32 +0000

    Sep 2, 2026, 9:19 PM

--- MySQL/F1.sql
+++ MySQL/F1.sql
@@ -1,2 +1,13 @@
 CREATE DATABASE hydra;
 USE hydra;
+
+CREATE TABLE users (
+users table:
+id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    gender ENUM('Male', 'Female', 'Other'),
+    date_of_birth DATE,
+);
+
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
