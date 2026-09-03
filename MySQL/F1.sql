53f18151352b8564f3bdca0294a3ab0c3e248479
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-09-03 04:40:22 +0000

    Sep 3, 2026, 10:10 AM

--- MySQL/F1.sql
+++ MySQL/F1.sql
@@ -77,7 +77,7 @@ FROM users
 WHERE
     date_of_birth BETWEEN '1990-01-01' AND '2000-12-31';
 
-    SELECT * FROM users WHERE name LIKE 'A%';
+SELECT * FROM users WHERE name LIKE 'A%';
 -- Starts with A
 SELECT * FROM users WHERE name LIKE '%a';
 -- Ends with a
