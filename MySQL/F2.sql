f75d8a25a9c9f9b9a431f9e4bcbc8d74c8449099
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-09-03 05:08:55 +0000

    Sep 3, 2026, 10:38 AM

--- MySQL/F2.sql
+++ MySQL/F2.sql
@@ -28,4 +28,8 @@ VALUES (
 FROM users
     INNER JOIN addresses ON users.id = addresses.user_id;
 
+    SELECT users.name, addresses.city
+FROM users
+    LEFT JOIN addresses ON users.id = addresses.user_id;
+
     
